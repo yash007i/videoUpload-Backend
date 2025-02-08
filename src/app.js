ba702be9bd975e4c7d2337949e185d7d93271534
yash007i <pyash2268@gmail.com>
2025-02-08 11:26:28 +0530

    All setup upto controllers

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express()
+
+//midleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//Config
+app.use(express.json({
+    limit: "16kb"
+}))
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+app.use(express.static("public")) // public assest exes by ever one
+
+// Handle user cookies and perform CURD ope ration
+app.use(cookieParser())
+
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+// router decalration
+app.use("/api/v1/users", userRouter)
+
+
+export { app }
---
+++ src/models/user.models.js
@@ -0,0 +1,96 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"; // bearrtoken mean jisne token bheja vo sahi
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({username:{
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index:true
+},
+email:{
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+},
+fullName:{
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+},
+avatar:{
+    type: String, // cloudinary url
+    required: true,
+},
+coverImage:{
+    type: String, // cloudinary url
+},
+watchHistory:[
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    }
+],
+password:{
+    type: String,
+    required: [true, 'Password is required'],
+},
+refreshToken:{
+    type: String
+}
+},
+{
+timestamps: true
+})
+
+
+//hooks, middleware
+userSchema.pre("save", async function (next)
+    {   
+        if(!this.isModified("password")){
+            return next();
+        }
+         this.password = await bcrypt.hash(this.password, 10)
+        next()
+    }
+)
+
+// custom method (bcrypt and jwt)
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken =  function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken =  function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
