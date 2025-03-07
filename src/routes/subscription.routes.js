afe45a89784fc072ea15080a946e003dc980a1f2
yash007i <pyash2268@gmail.com>
2025-03-07 22:32:19 +0530

    add routes for comments, likes, playlists, subscriptions, tweets, and videos with JWT authentication

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/c/:channelId").post(toggleSubscription);
+
+router.route("/u/:channelId").get(getUserChannelSubscribers);
+
+router.route("/c/:subscriberId").get(getSubscribedChannels);
+
+export default router;
